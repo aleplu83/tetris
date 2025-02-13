252260a2d54d8f5e7980fd3e599bd9afa95cd971
Alessandro Pluchino <aleplu@itpla.be>
2025-02-13 12:11:11 +0100

    commit

---
+++ Tetris/src/main/MainWindow.java
@@ -0,0 +1,14 @@
+package main;
+
+public class MainWindow {
+
+	public MainWindow() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
