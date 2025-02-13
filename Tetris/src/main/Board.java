101f47058891de35a7c643085e2eb422d449e7db
Alessandro Pluchino <aleplu@itpla.be>
2025-02-13 12:49:18 +0100

    added files

---
+++ Tetris/src/main/Board.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+public class Board extends JPanel {
+
+	public Board() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Board(LayoutManager layout) {
+		super(layout);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Board(boolean isDoubleBuffered) {
+		super(isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Board(LayoutManager layout, boolean isDoubleBuffered) {
+		super(layout, isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Tetris/src/main/Brick.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Brick {
+
+	public Brick() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
