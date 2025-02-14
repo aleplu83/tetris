02dbb13408c495d6c269a0c6b801a9158ad3e649
Alessandro Pluchino <aleplu@itpla.be>
2025-02-14 11:52:39 +0100

    create main window and panel

--- Tetris/src/main/MainWindow.java
+++ Tetris/src/main/MainWindow.java
@@ -1,16 +1,30 @@
 package main;
 
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
 public class MainWindow {
 
+	private JFrame window;
+	private Board board;
+	
 	public MainWindow() {
 		// TODO Auto-generated constructor stub
+		window = new JFrame("Tetris");
+		board = new Board();
+		window.add(board);
+		board.setBackground(Color.BLACK);
+		window.setSize(500, 800);
+		window.setLocation(700, 200);
+		window.setVisible(true);
 	}
 	
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new MainWindow();
 	}
 
 }
